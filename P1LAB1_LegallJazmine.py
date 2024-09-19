38275c2144ea5cdd30370cbb4bb6e9af97ea540e
ApolloSnow <jazminelele@gmail.com>
2024-09-18 22:26:18 -0400

    Add files via upload
    First submission,

---
+++ P1LAB1_LegallJazmine.py
@@ -0,0 +1,13 @@
+#Jazmine Legall
+#09/17/2024
+#P1LAB1
+#Get input from user and display output
+
+#Get First Name
+firstName = input(" Enter your first name: ")
+#Get Last Name
+
+lastName = input("Enter your last name:  ")
+
+# Print to console
+print("Hello, ",  firstName, lastName + "! Welcome to CTI-110")
